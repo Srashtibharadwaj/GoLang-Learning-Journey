0d628b280bd5414661b95ab285adea72dd7fe780
Srashtibharadwaj <srashti1102005@gmail.com>
2026-09-20 13:23:39 +0530

    Add files via upload

---
+++ Day-03/Arrays/2Darrays/2Darray.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	nums := [2][2]int{{5, 6}, {5, 6}}
+	fmt.Println(nums)
+}
---
+++ Day-03/Arrays/arrays.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	var nums [4]int
+	//fmt.Println(len(nums))
+	nums[0] = 1
+	fmt.Println(nums)
+}
