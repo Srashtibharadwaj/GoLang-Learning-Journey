c029be269731bd880a26ed3ee86a6d019105cae6
Srashtibharadwaj <srashti1102005@gmail.com>
2026-09-20 13:11:42 +0530

    Add files via upload

---
+++ Day-02/Forloop/forloop.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	//i := 1
+	//for i <= 3 {
+	//fmt.Println(i)
+	//i = i + 1
+	//}
+	for i := 0; i <= 3; i++ {
+		if i == 2 {
+			continue
+		}
+		fmt.Println(i)
+	}
+}
---
+++ Day-02/Forloop/ifelse.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	//var role = "Admin"
+	//var permission = false
+	//if role == "Admin" && permission {
+	//	fmt.Println("Yes")
+	//}
+	if Age := 15; Age >= 18 {
+		fmt.Println("Person is an adult", Age)
+	} else if Age >= 12 {
+		fmt.Println("Person is teenagers", Age)
+	}
+}
