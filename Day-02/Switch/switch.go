afa3097be66dc98bd017f1594e8dcb6aca12d11e
Srashtibharadwaj <srashti1102005@gmail.com>
2026-09-20 13:12:19 +0530

    Add files via upload

---
+++ Day-02/Switch/switch.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	i := 5
+
+	switch i {
+	case 1:
+		fmt.Println("One")
+	case 2:
+		fmt.Println("Two")
+	case 3:
+		fmt.Println("Three")
+	default:
+		fmt.Println("Nothing")
+	}
+}
